c1cab1195788a20d0d3174e6c3cebe3e5a74e2c7
yuffiy <yfhj1990@hotmail.com>
2017-03-14 14:06:40 +0800

    Fix #43. Add `string` options to output json object string.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
 */
+var loaderUtils = require('loader-utils');
+
 module.exports = function(source) {
-	this.cacheable && this.cacheable();
-	var value = typeof source === "string" ? JSON.parse(source) : source;
-	this.value = [value];
-	return "module.exports = " + JSON.stringify(value) + ";";
+    this.cacheable && this.cacheable();    
+    var value = typeof source === "string" ? JSON.parse(source) : source;
+    this.value = [value];
+    var query = loaderUtils.getOptions(this) || {}
+    if(query.string)
+	return "module.exports = `" + JSON.stringify(value) + "`;";
+    return "module.exports = " + JSON.stringify(value) + ";";
 }
