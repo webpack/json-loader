0874a9c946942dd378446e24305509ea72acad46
Tobias Koppers <tobias.koppers@googlemail.com>
2017-05-08 00:35:09 +0200

    Revert "fix: add `stringify` option to output JSON object as string (#45)"
    This reverts commit 84681978038ed8ef516fe008406599e5ecb76ec7.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 /*
-  MIT License http://www.opensource.org/licenses/mit-license.php
-  Author Tobias Koppers @sokra
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
 */
-var loaderUtils = require('loader-utils');
-
 module.exports = function(source) {
-    var value = typeof source === "string" ? JSON.parse(source) : source;
-    var options = loaderUtils.getOptions(this) || {};
-    value = JSON.stringify(value)
-    value = options.stringify ? `'${value}'` : value
-    var module = this.version && this.version >= 2 ? `export default ${value};` : `module.exports = ${value};`;
-    return module
+	this.cacheable && this.cacheable();
+	var value = typeof source === "string" ? JSON.parse(source) : source;
+	this.value = [value];
+	return "module.exports = " + JSON.stringify(value) + ";";
 }
